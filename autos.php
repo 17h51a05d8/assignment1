1b45781e227c352e385e4dced07929c598f2a7e1
17h51a05d8 <aishwaryagumastha18@gmail.com>
2020-05-05 16:32:44 +0530

    first assignment

--- autos.php
+++ autos.php
@@ -77,7 +77,7 @@ while ( $row = $all_autos->fetch(PDO::FETCH_OBJ) )
 <!DOCTYPE html>
 <html>
     <head>
-        <title>GUMASTHA AISHWARYA Autos</title>
+        <title>Gumastha Aishwarya Autos</title>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
     <body>
--- login.php
+++ login.php
@@ -51,7 +51,7 @@ if ( isset($_POST['who']) && isset($_POST['pass']) )
 <!DOCTYPE html>
 <html>
     <head>
-        <title>GUMASTHA AISHWARYA Login Page</title>
+        <title>Gumastha Aishwarya Login Page</title>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
     <body>
